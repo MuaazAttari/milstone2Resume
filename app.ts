7011416a447f698477cf9b5c722a9d3ceffd8b60
MuaazAttari <160422454+MuaazAttari@users.noreply.github.com>
2024-09-08 17:02:44 +0500

    Add files via upload

--- app.ts
+++ app.ts
@@ -1,10 +1,14 @@
-const toggleButton = document.getElementById('toggleSkills') as HTMLButtonElement | null;
-const skillsSection = document.querySelector('.skills') as HTMLElement | null;
+const toggleButton = document.getElementById(
+  "toggleSkills"
+) as HTMLButtonElement | null;
 
-toggleButton?.addEventListener('click', () => {
-    if (skillsSection?.classList.contains('hidden')) {
-        skillsSection.classList.remove('hidden');
-    } else {
-        skillsSection?.classList.add('hidden');
-    }
+const skillsSection = document.querySelector(".skills") as HTMLElement | null;
+
+toggleButton?.addEventListener("click", () => {
+  if (skillsSection?.classList.contains("hidden")) {
+    skillsSection.classList.remove("hidden");
+  }
+    else {
+    skillsSection?.classList.add("hidden");
+  }
 });
